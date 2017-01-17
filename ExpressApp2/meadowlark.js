fabf95b6852842a35533736b729edd76c3245af3
Alex Kennedy <Alex Kennedy@KEN02-AK>
2017-01-17 17:06:00 +0000

    Add project files.

---
+++ ExpressApp2/meadowlark.js
@@ -0,0 +1,92 @@
+﻿
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+/*
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+*/
+
+var app = express();
+
+// set up handlebars view engine
+var handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+
+/*
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+*/
+
+
+var fortunes = [
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple.",
+];
+
+
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.get('/about', function (req, res) {
+    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+    console.log('Random Fortune:' + randomFortune);
+    res.render('about', {fortune: randomFortune});
+}); 
+
+
+// custom 404 page
+app.use(function (req, res,next) {
+    res.status(404);
+    res.render('404');
+});
+
+    // custom 404 page
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+/*
+// development only
+if ('development' == app.get('env')) {
+    app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/about', routes.about);
+app.get('/contact', routes.contact);
+
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
+
+*/
+
+app.listen(app.get('port'), function () {
+    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
